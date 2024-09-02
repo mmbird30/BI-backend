e6a0383d3437d15a65e61c76d69b4ea4a399aafb
mmbird30 <19120021986@163.com>
2024-09-02 17:27:17 +0800

    init

---
+++ src/main/java/com/mmbird/springbootinit/mapper/UserMapper.java
@@ -0,0 +1,17 @@
+package com.mmbird.springbootinit.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.mmbird.springbootinit.model.entity.User;
+
+/**
+ * 用户数据库操作
+ *
+ *
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
