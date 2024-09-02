8a3bbb7ce473e1ca8e101272b22a0dfcc13380f0
mmbird30 <19120021986@163.com>
2024-09-02 19:00:10 +0800

    BI-backend init

---
+++ src/main/java/com/mmbird/bi/controller/PostThumbController.java
@@ -0,0 +1,55 @@
+package com.mmbird.bi.controller;
+
+import com.mmbird.bi.common.BaseResponse;
+import com.mmbird.bi.common.ErrorCode;
+import com.mmbird.bi.common.ResultUtils;
+import com.mmbird.bi.exception.BusinessException;
+import com.mmbird.bi.model.dto.postthumb.PostThumbAddRequest;
+import com.mmbird.bi.model.entity.User;
+import com.mmbird.bi.service.PostThumbService;
+import com.mmbird.bi.service.UserService;
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 帖子点赞接口
+ *
+ *
+ */
+@RestController
+@RequestMapping("/post_thumb")
+@Slf4j
+public class PostThumbController {
+
+    @Resource
+    private PostThumbService postThumbService;
+
+    @Resource
+    private UserService userService;
+
+    /**
+     * 点赞 / 取消点赞
+     *
+     * @param postThumbAddRequest
+     * @param request
+     * @return resultNum 本次点赞变化数
+     */
+    @PostMapping("/")
+    public BaseResponse<Integer> doThumb(@RequestBody PostThumbAddRequest postThumbAddRequest,
+            HttpServletRequest request) {
+        if (postThumbAddRequest == null || postThumbAddRequest.getPostId() <= 0) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR);
+        }
+        // 登录才能点赞
+        final User loginUser = userService.getLoginUser(request);
+        long postId = postThumbAddRequest.getPostId();
+        int result = postThumbService.doPostThumb(postId, loginUser);
+        return ResultUtils.success(result);
+    }
+
+}
